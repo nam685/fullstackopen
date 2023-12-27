0108d85b401b8fb2616dd7db8d1a13f64f5f982b
Nam Le <lehainam2371999@gmail.com>
2023-12-27 21:42:49 +0700

    Part 1 - Exercise 1.5: Course Info

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,51 @@
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
+
+const Content = (props) => (
+  <>
+    <Part part={props.parts[0]}></Part>
+    <Part part={props.parts[1]}></Part>
+    <Part part={props.parts[2]}></Part>
+  </>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => (
+  <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+)
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <>
+      <Header course={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
+    </>
+  )
+}
+
+export default App
